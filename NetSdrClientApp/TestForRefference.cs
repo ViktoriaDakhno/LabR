18cffd985b1c71a56964f688725c2d5a6daac3cb
Victoria Dakhno <8460185@stud.nau.edu.ua>
2025-11-04 18:11:51 +0200

    Додано пряму залежність між NetSdrClientApp і EchoTspServer. Створено клас Dummy у UI для демонстрації цієї залежності. Додано тестовий клас ArchitectureTests з правилом перевірки, щоб тест спеціально провалювався при наявності прямої залежності.

---
+++ NetSdrClientApp/TestForRefference.cs
@@ -0,0 +1,12 @@
+﻿using EchoServer;
+
+namespace NetSdrClientApp
+{
+    public class TestForRefference
+    {
+        public void CreateServer()
+        {
+            var server = new EchoServer.EchoServer(5000);
+        }
+    }
+}
--- NetSdrClientAppTests/ArchitectureTests.cs
+++ NetSdrClientAppTests/ArchitectureTests.cs
@@ -1,54 +1,23 @@
 using NetArchTest.Rules;
 using NUnit.Framework;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 
 namespace NetSdrClientAppTests
 {
     public class ArchitectureTests
     {
         [Test]
-        public void App_Should_Not_Depend_On_EchoServer()
+        public void UI_ShouldNotDependOnInfrastructureDirectly()
         {
-            var result = Types.InAssembly(typeof(NetSdrClientApp.NetSdrClient).Assembly)
+            var assembly = typeof(NetSdrClientApp.NetSdrClient).Assembly;
+
+            var result = Types.InAssembly(assembly)
                 .That()
                 .ResideInNamespace("NetSdrClientApp")
                 .ShouldNot()
                 .HaveDependencyOn("EchoServer")
                 .GetResult();
 
-            Assert.That(result.IsSuccessful, Is.True);
-        }
-
-        [Test]
-        public void Messages_Should_Not_Depend_On_Networking()
-        {
-            // Arrange
-            var result = Types.InAssembly(typeof(NetSdrClientApp.Messages.NetSdrMessageHelper).Assembly)
-                .That()
-                .ResideInNamespace("NetSdrClientApp.Messages")
-                .ShouldNot()
-                .HaveDependencyOn("NetSdrClientApp.Networking")
-                .GetResult();
-
-            // Assert
-            Assert.That(result.IsSuccessful, Is.True);
-        }
-
-        [Test]
-        public void Networking_Should_Not_Depend_On_Messages()
-        {
-            // Arrange
-            var result = Types.InAssembly(typeof(NetSdrClientApp.Networking.ITcpClient).Assembly)
-                .That()
-                .ResideInNamespace("NetSdrClientApp.Networking")
-                .ShouldNot()
-                .HaveDependencyOn("NetSdrClientApp.Messages")
-                .GetResult();
-
-            // Assert
-            Assert.That(result.IsSuccessful, Is.True);
+            Assert.IsTrue(result.IsSuccessful);
         }
     }
 }
