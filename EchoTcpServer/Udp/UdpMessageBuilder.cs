56637292727394c0ce13d5e0e6f50d572ae7895d
Victoria Dakhno <8460185@stud.nau.edu.ua>
2025-11-05 15:11:19 +0200

    Покращення архітектури сервера Echo та тестованості У Program.cs внесено зміни для підтримки впровадження залежностей з використанням ITcpListenerWrapper та додано клас EchoClientHandler для обробки клієнтів.
    Додано нові інтерфейси та класи у EchoClientHandler.cs, TcpListenerWrapper.cs, IClientHandler.cs, ITcpListenerWrapper.cs, IUdpSender.cs, EchoServer.cs, UdpMessageBuilder.cs та UdpTimedSender.cs, що покращує структуру коду та розділення відповідальностей.
    
    У UdpTimedSender реалізовано можливість використання UdpMessageBuilder для створення повідомлень.
    
    Зміни спрямовані на покращення структури коду, впровадження принципів SOLID та спрощення тестування.

---
+++ EchoTcpServer/Udp/UdpMessageBuilder.cs
@@ -0,0 +1,30 @@
+﻿namespace EchoServer.Udp
+{
+    /// <summary>
+    /// Builds UDP messages with header and payload
+    /// </summary>
+    public class UdpMessageBuilder
+    {
+        private ushort _sequenceNumber = 0;
+        private readonly Random _random;
+
+        public UdpMessageBuilder()
+        {
+            _random = new Random();
+        }
+
+        public byte[] BuildMessage()
+        {
+            byte[] samples = new byte[1024];
+            _random.NextBytes(samples);
+            _sequenceNumber++;
+
+            byte[] header = new byte[] { 0x04, 0x84 };
+            byte[] sequence = BitConverter.GetBytes(_sequenceNumber);
+
+            return header.Concat(sequence).Concat(samples).ToArray();
+        }
+
+        public ushort GetCurrentSequenceNumber() => _sequenceNumber;
+    }
+}
