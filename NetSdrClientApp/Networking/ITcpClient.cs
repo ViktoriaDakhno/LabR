d68cb6fbce0ffcc7ca2c90a6232e722ea61842a6
ViktoriaDakhno <8460185@stud.nau.edu.ua>
2025-11-20 10:18:42 +0200

    Change namespace for ITcpClient interface

--- NetSdrClientApp/Networking/ITcpClient.cs
+++ NetSdrClientApp/Networking/ITcpClient.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
-namespace NetSdrClientApp.Networking
+namespace NetSdrClientApp
 {
     public interface ITcpClient
     {
